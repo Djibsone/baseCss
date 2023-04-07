2063ceb1a958ba71f1a805c3a844c1ec2037a40a
Djibsone <djibriltamou@gmail.com>
2023-04-07 11:49:00 +0100

    initialisation

---
+++ new.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="site">
+        <div id="banner1">
+            <p>TDJIB.COM</p>
+        </div>
+        <div id="menu">
+            <ul>
+                <li><a href="./index.php">ACCUEIL</a></li>
+                <li><a href="./index.php">NEWS</a></li>
+                <li><a href="#">INFOS</a></li>
+                <li><a href="#">CONTACT</a></li>
+            </ul>
+        </div>
+        <div id="banner2"></div>
+
+        <div id="contenuprincipal">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis voluptatum accusamus consectetur. Quisquam laborum dolorum, sed unde voluptatem nisi magni? Eum architecto, pariatur iusto quae reiciendis voluptas, iure quasi fugit quaerat quo odit vero eos perspiciatis iste. Ratione veniam dolor totam vitae, repellendus quas animi vero tempora repellat mollitia dolorum odit officia est illo asperiores maxime autem in nihil beatae ipsa consequuntur neque sapiente reprehenderit? Eum excepturi praesentium molestias aliquam in, sequi quos reiciendis nisi nobis facilis. Harum saepe pariatur debitis, deleniti accusamus architecto eligendi fugiat ad dolor laboriosam. Quos dolorem architecto quasi atque maxime incidunt eos, provident aperiam quia a quibusdam facere beatae repellendus quam ea omnis nemo tempore aliquid fugiat eum recusandae, dolorum totam. Delectus veniam laboriosam dolore! Saepe rem voluptatum praesentium! Provident ullam consequatur saepe! Optio architecto incidunt cupiditate modi. Harum commodi quos repellat, eveniet, ipsa deleniti quis voluptatem inventore a rerum ipsum. Repellendus dolor officiis magni assumenda odio laboriosam corporis, recusandae culpa ut fugit deleniti unde consectetur nostrum. Aliquam reiciendis a autem, dolorem ratione tempora laborum voluptates, fuga neque nulla voluptate fugiat quos numquam temporibus iusto odit. Tempora consequuntur autem provident dolorem error obcaecati enim alias totam dignissimos beatae et quia fugiat ut dolorum id aliquam, illo ratione, sunt perferendis numquam laborum! Vitae doloremque rem vel, distinctio quidem est placeat quam recusandae ab! Vitae natus necessitatibus in dolorem officia asperiores dolores voluptatem libero? Voluptates labore ab explicabo enim sed sapiente consectetur vitae est necessitatibus natus, nulla cum voluptatum minima delectus voluptatem ducimus illo ad, rem aliquam, nisi voluptas praesentium ipsa distinctio quis? Voluptatem, eos? Perferendis harum pariatur nemo, asperiores aliquid in id veniam facilis fugiat. Ab illum necessitatibus rerum magni corrupti et id similique dolor, tenetur blanditiis vitae dolorem repudiandae deleniti autem, voluptates unde perspiciatis quia ad.</p> 
+        </div>
+
+        <div id="footer">
+            <p>TD | Tous les droits réservés</p>
+        </div>
+    </div>
+</body>
+</html>
